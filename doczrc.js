dda86cfc49944fc00824cd8722bed5d61be7ce35
j8seangel <jseangel@gmail.com>
2019-02-01 17:18:38 +0100

    Doczr theme and root page

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,15 @@ import { css } from 'docz-plugin-css'
 
 export default {
   hashRouter: true,
+  title: 'GFW UI components',
+  description: 'UI components used in Global Fishing Watch',
+  base: '/map-components/',
+  themeConfig: {
+    colors: {
+      primary: '#e54430',
+      text: '#33447e',
+    },
+  },
   plugins: [
     css({
       preprocessor: 'postcss',
