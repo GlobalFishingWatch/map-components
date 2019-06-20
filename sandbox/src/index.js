79b160002ccfe863fc608fe4d8428b984ae6e8b8
j8seangel <jseangel@gmail.com>
2019-06-20 10:53:52 +0200

    add polyfills for ie in sandbox

--- sandbox/src/index.js
+++ sandbox/src/index.js
@@ -1,6 +1,10 @@
+import 'react-app-polyfill/ie11'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+
+// Polyfill for older browsers (IE11 for example)
+window.Promise = window.Promise || Promise
 
 ReactDOM.render(<App />, document.getElementById('root'))
