0f404a8aecbe79d57c3d3c9e3c9eaa450aab308e
nerik <erik.escoffier@gmail.com>
2019-07-18 11:15:29 +0200

    add week labels

--- src/timebar/layouts/index.js
+++ src/timebar/layouts/index.js
@@ -3,11 +3,35 @@ import { getTime } from '../utils'
 
 const getUnitLabel = (mUnit, baseUnit, availableWidth) => {
   /* eslint key-spacing: 0, no-multi-spaces: 0 */
+
+  const getWeekFmt = (mUnit, isFirst = false) => {
+    const mWeekEnd = mUnit.add(6, 'day')
+    return `${mUnit.format('MMM')} ${mUnit.format('D')}-${mWeekEnd.format('D')} ${
+      isFirst ? mUnit.format('YYYY') : ''
+    }`
+  }
+
   const FORMATS = {
     year: { isFirst: () => false, formats: [[0, 'YYYY']] },
     month: {
       isFirst: (fm) => fm.month() === 0,
       formats: [[200, 'MMMM YYYY'], [100, 'MMMM', 'MMM YYYY'], [0, 'MMM', 'MMM YY']],
+    },
+    week: {
+      isFirst: (fm) => {
+        return fm.date() === 1
+      },
+      formats: [
+        [
+          0,
+          (mUnit) => {
+            return getWeekFmt(mUnit)
+          },
+          (mUnit) => {
+            return getWeekFmt(mUnit, true)
+          },
+        ],
+      ],
     },
     day: {
       isFirst: (fm) => fm.date() === 1,
@@ -34,8 +58,8 @@ const getUnitLabel = (mUnit, baseUnit, availableWidth) => {
   }
 
   const isFirst = unitFormat.isFirst(mUnit)
-  const formatString = isFirst && format[2] ? format[2] : format[1]
-  return mUnit.format(formatString)
+  const finalFormat = isFirst && format[2] ? format[2] : format[1]
+  return typeof finalFormat === 'function' ? finalFormat(mUnit) : mUnit.format(finalFormat)
 }
 
 export const getUnitsPositions = (
@@ -68,6 +92,7 @@ export const getUnitsPositions = (
       {
         year: 'YYYY',
         month: 'YYYY-MM',
+        week: 'YYYY-MM-DD',
         day: 'YYYY-MM-DD',
         hour: 'YYYY-MM-DD-HH',
       }[baseUnit]
