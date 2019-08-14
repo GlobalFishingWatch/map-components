aa73d37b7e86ee37a8209eb478c8a0a68d2566d4
j8seangel <jseangel@gmail.com>
2019-08-14 12:22:48 +0200

    layer-manager typing

---
+++ types/components/layer-manager.d.ts
@@ -0,0 +1,31 @@
+import React from 'react'
+import { AnySourceImpl, Layer } from '@types/mapbox-gl'
+
+export interface LayerManagerGenerator {
+  type: string
+  getStyleSources: (layer: LayerManagerLayer) => AnySourceImpl
+  getStyleLayers: (layer: LayerManagerLayer) => Layer
+}
+
+export interface LayerManagerOptions {
+  generators: { [key: string]: LayerManagerGenerator }
+  version: string
+  glyphs: string
+}
+
+export interface LayerManagerLayer {
+  id: string
+  type: 'background' | 'basemap' | 'carto'
+  visible?: boolean
+  opacity?: number
+  color?: string
+}
+
+export interface LayerManagerComponent {
+  config?: LayerManagerOptions
+  layers: LayerManagerLayer[]
+}
+
+export default class MapModule extends React.Component<LayerManagerComponent, any> {
+  render(): JSX.Element
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,12 @@ export { default as CountryFlag } from './components/countryflag'
 export { default as Miniglobe } from './components/miniglobe'
 export { default as Timebar } from './components/timebar'
 export { default as Map } from './components/map'
+export { default as LayerManager } from './components/layer-manager'
+export {
+  LayerManagerGenerator,
+  LayerManagerOptions,
+  LayerManagerLayer,
+} from './components/layer-manager'
 
 // declare module '@globalfishingwatch/map-components/components/countryflag' {
 //   export = CountryFlag;
