4fe17eee5a8fa36691566832f1fbd33b65d59261
nerik <erik.escoffier@gmail.com>
2019-08-30 17:54:00 +0200

    fix bookmark anim + move to functional comp

--- src/timebar/components/bookmark.js
+++ src/timebar/components/bookmark.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { getHumanizedDates } from '../utils'
@@ -12,76 +12,72 @@ const MIN_WIDTH = 32
 const MIN_WIDTH_WITH_OVERFLOW_ARROWS = 52
 const COMPACT_MAX_WIDTH = 240
 
-class Bookmark extends Component {
-  static contextType = ImmediateContext
-  render() {
-    const { scale, bookmarkStart, bookmarkEnd, minX, maxX, onSelect } = this.props
-    const { immediate } = this.context
+const Bookmark = ({ scale, bookmarkStart, bookmarkEnd, minX, maxX, onSelect, onDelete }) => {
+  const { immediate } = useContext(ImmediateContext)
 
-    const x = scale(new Date(bookmarkStart))
-    const width = scale(new Date(bookmarkEnd)) - x
-    const { humanizedStart, humanizedEnd } = getHumanizedDates(bookmarkStart, bookmarkEnd)
-    const label = [humanizedStart, humanizedEnd].join(' - ')
+  const x = scale(new Date(bookmarkStart))
+  const width = scale(new Date(bookmarkEnd)) - x
+  const { humanizedStart, humanizedEnd } = getHumanizedDates(bookmarkStart, bookmarkEnd)
+  const label = [humanizedStart, humanizedEnd].join(' - ')
 
-    let overflowing
-    let overflowingLeft
-    let overflowingRight
-    let renderedX = x
-    let renderedWidth = width
+  let overflowing
+  let overflowingLeft
+  let overflowingRight
+  let renderedX = x
+  let renderedWidth = width
 
-    if (x < minX) {
-      renderedX = minX
-      renderedWidth = x + renderedWidth
-      overflowing = true
-      overflowingLeft = true
-    }
-    if (renderedX + width > maxX) {
-      renderedX = Math.min(renderedX, maxX - MIN_WIDTH_WITH_OVERFLOW_ARROWS)
-      renderedWidth = maxX - renderedX
-      overflowing = true
-      overflowingRight = true
-    }
+  if (x < minX) {
+    renderedX = minX
+    renderedWidth = x + renderedWidth
+    overflowing = true
+    overflowingLeft = true
+  }
+  if (renderedX + width > maxX) {
+    renderedX = Math.min(renderedX, maxX - MIN_WIDTH_WITH_OVERFLOW_ARROWS)
+    renderedWidth = maxX - renderedX
+    overflowing = true
+    overflowingRight = true
+  }
 
-    const minWidth = overflowing === true ? MIN_WIDTH_WITH_OVERFLOW_ARROWS : MIN_WIDTH
-    renderedWidth = Math.max(minWidth, renderedWidth)
-    const compact = renderedWidth < COMPACT_MAX_WIDTH
-    const point = renderedWidth <= MIN_WIDTH
+  const minWidth = overflowing === true ? MIN_WIDTH_WITH_OVERFLOW_ARROWS : MIN_WIDTH
+  renderedWidth = Math.max(minWidth, renderedWidth)
+  const compact = renderedWidth < COMPACT_MAX_WIDTH
+  const point = renderedWidth <= MIN_WIDTH
 
-    return (
-      <div
-        className={classNames(styles.Bookmark, {
-          [styles._overflowingLeft]: overflowingLeft,
-          [styles._overflowingRight]: overflowingRight,
-          [styles._compact]: compact,
-          [styles._point]: point,
-          [styles._immediate]: immediate,
-        })}
-        style={{ left: renderedX, width: renderedWidth }}
+  return (
+    <div
+      className={classNames(styles.Bookmark, {
+        [styles._overflowingLeft]: overflowingLeft,
+        [styles._overflowingRight]: overflowingRight,
+        [styles._compact]: compact,
+        [styles._point]: point,
+        [styles._immediate]: immediate,
+      })}
+      style={{ left: renderedX, width: renderedWidth }}
+    >
+      <button
+        type="button"
+        title="Go to time range bookmark"
+        className={styles.main}
+        onClick={onSelect}
       >
+        {overflowingLeft && <Arrow className={styles.leftArrow} />}
+        <IconBookmarkFilled className={styles.icon} />
+        {compact === false && <span className={styles.title}>{label}</span>}
+        {overflowingRight && <Arrow className={styles.rightArrow} />}
+      </button>
+      {compact === false && (
         <button
           type="button"
-          title="Go to time range bookmark"
-          className={styles.main}
-          onClick={onSelect}
+          title="Delete time range bookmark"
+          className={styles.delete}
+          onClick={onDelete}
         >
-          {overflowingLeft && <Arrow className={styles.leftArrow} />}
-          <IconBookmarkFilled className={styles.icon} />
-          {compact === false && <span className={styles.title}>{label}</span>}
-          {overflowingRight && <Arrow className={styles.rightArrow} />}
+          <IconDelete />
         </button>
-        {compact === false && (
-          <button
-            type="button"
-            title="Delete time range bookmark"
-            className={styles.delete}
-            onClick={this.props.onDelete}
-          >
-            <IconDelete />
-          </button>
-        )}
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
 
 Bookmark.propTypes = {
@@ -90,7 +86,6 @@ Bookmark.propTypes = {
   scale: PropTypes.func.isRequired,
   minX: PropTypes.number.isRequired,
   maxX: PropTypes.number.isRequired,
-  immediate: PropTypes.bool.isRequired,
   onSelect: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
 }
