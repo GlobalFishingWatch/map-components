72552fbb556009f77ce8a147355685575bcebf98
nerik <erik.escoffier@gmail.com>
2019-09-19 12:31:29 +0200

    removed legacy tracks rendering

--- src/map/tracks/tracks.actions.js
+++ src/map/tracks/tracks.actions.js
@@ -2,69 +2,12 @@ import cloneDeep from 'lodash/cloneDeep'
 import { targetMapVessel } from '../store'
 import { getTrackBounds, getTrackTimeBounds } from '..'
 
-import {
-  getTilePromises,
-  getCleanVectorArrays,
-  groupData,
-  addTracksPointsRenderingData,
-  getTracksPlaybackData,
-} from '../utils/heatmapTileData'
+import { getTilePromises, getCleanVectorArrays, groupData } from '../utils/heatmapTileData'
 import { startLoader, completeLoader } from '../module/module.actions'
 
 export const ADD_TRACK = 'ADD_TRACK'
 export const UPDATE_TRACK = 'UPDATE_TRACK'
 export const REMOVE_TRACK = 'REMOVE_TRACK'
-
-// Deprecated tracks format parsing
-const getOldTrackBoundsFormat = (data, addOffset = false) => {
-  const time = {
-    start: Infinity,
-    end: 0,
-  }
-  const geo = {
-    minLat: Infinity,
-    maxLat: -Infinity,
-    minLng: Infinity,
-    maxLng: -Infinity,
-  }
-  for (let i = 0, length = data.datetime.length; i < length; i++) {
-    const datetime = data.datetime[i]
-    if (datetime < time.start) {
-      time.start = datetime
-    } else if (datetime > time.end) {
-      time.end = datetime
-    }
-
-    const lat = data.latitude[i]
-    if (lat < geo.minLat) {
-      geo.minLat = lat
-    } else if (lat > geo.maxLat) {
-      geo.maxLat = lat
-    }
-
-    let lng = data.longitude[i]
-    if (addOffset === true) {
-      if (lng < 0) {
-        lng += 360
-      }
-    }
-    if (lng < geo.minLng) {
-      geo.minLng = lng
-    } else if (lng > geo.maxLng) {
-      geo.maxLng = lng
-    }
-  }
-
-  // track crosses the antimeridian
-  if (geo.maxLng - geo.minLng > 350 && addOffset === false) {
-    return getOldTrackBoundsFormat(data, true)
-  }
-
-  return {
-    time: [time.start, time.end],
-    geo,
-  }
-}
 
 const convertLegacyTrackToGeoJSON = (vectorArrays) => {
   const createFeature = (segId, type = 'track', geomType = 'LineString') => ({
@@ -175,20 +118,17 @@ function loadTrack(track) {
           'sigma',
         ])
 
-        const vectorArray = addTracksPointsRenderingData(rawTrackData)
-        const bounds = getOldTrackBoundsFormat(rawTrackData)
-
-        //const data = getTracksPlaybackData(vectorArray)
-
-        const geoJSON = convertLegacyTrackToGeoJSON(vectorArray)
+        const data = convertLegacyTrackToGeoJSON(rawTrackData)
+        const timelineBounds = getTrackTimeBounds(data)
+        const geoBounds = getTrackBounds(data)
 
         dispatch({
           type: UPDATE_TRACK,
           payload: {
             id,
-            data: geoJSON,
-            geoBounds: bounds.geo,
-            timelineBounds: bounds.time,
+            data,
+            geoBounds,
+            timelineBounds,
           },
         })
         dispatch(completeLoader(loaderID))
