cf089eba27e4359148e81afd11bd014edd871406
erik <erik@satellitestud.io>
2020-04-16 17:44:09 +0200

    added tooltip

--- src/timebar/charts/tracks-events.js
+++ src/timebar/charts/tracks-events.js
@@ -1,9 +1,63 @@
-import React, { useContext, useMemo } from 'react'
+import React, { useContext, useState, useMemo, Fragment } from 'react'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
+import dayjs from 'dayjs'
 import styles from './tracks-events.module.css'
 import { getTrackY } from './utils'
-import { DEFAULT_CSS_TRANSITION } from '../constants'
+import { DEFAULT_CSS_TRANSITION, DEFAULT_FULL_DATE_FORMAT } from '../constants'
 import ImmediateContext from '../immediateContext'
+
+import { ReactComponent as IconEncounter } from '../icons/events/encounter.svg'
+import { ReactComponent as IconUnregistered } from '../icons/events/unregistered.svg'
+import { ReactComponent as IconGap } from '../icons/events/gap.svg'
+import { ReactComponent as IconPort } from '../icons/events/port.svg'
+
+const ICONS = {
+  encounter: <IconEncounter />,
+  unregistered: <IconUnregistered />,
+  gap: <IconGap />,
+  loitering: <IconUnregistered />,
+  port: <IconPort />,
+  fishing: null,
+}
+
+const Tooltip = ({ highlightedEvent, outerScale }) => {
+  if (!highlightedEvent) {
+    return null
+  }
+  const left = outerScale(highlightedEvent.start)
+  const width = highlightedEvent.end === null ? 0 : outerScale(highlightedEvent.end) - left
+  const center = left + width / 2
+
+  const start = dayjs(highlightedEvent.start)
+  return (
+    <div className={styles.tooltip} style={{ left: `${center}px` }} key="tooltip">
+      {ICONS[highlightedEvent.type]}
+      <div className={styles.tooltipText}>
+        <div className={styles.tooltipDate} style={{ color: highlightedEvent.color }}>
+          {start.format(DEFAULT_FULL_DATE_FORMAT)}
+        </div>
+        {highlightedEvent.description}
+      </div>
+    </div>
+  )
+}
+
+Tooltip.propTypes = {
+  highlightedEvent: PropTypes.shape({
+    type: PropTypes.string,
+    start: PropTypes.number,
+    end: PropTypes.number,
+    color: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  outerScale: PropTypes.func.isRequired,
+}
+
+Tooltip.defaultProps = {
+  highlightedEvent: null,
+}
 
 const getCoordinates = (tracksEvents, outerScale) => {
   return tracksEvents.map((trackEvents) => {
@@ -23,42 +77,54 @@ const getCoordinates = (tracksEvents, outerScale) => {
   })
 }
 
-const TracksEvents = ({ tracksEvents, outerWidth, graphHeight, outerScale }) => {
+const TracksEvents = ({ tracksEvents, outerWidth, graphHeight, outerScale, tooltipContainer }) => {
   const { immediate } = useContext(ImmediateContext)
   const tracksEventsWithCoordinates = useMemo(() => getCoordinates(tracksEvents, outerScale), [
     tracksEvents,
     outerScale,
   ])
+  const [highlightedEvent, setHighlightedEvent] = useState(null)
   return (
-    <div width={outerWidth} height={graphHeight} className={styles.Events}>
-      {tracksEventsWithCoordinates.map((trackEvents, index) => (
-        <div
-          key={index}
-          className={styles.track}
-          style={{
-            top: `${getTrackY(tracksEvents.length, index, graphHeight)}px`,
-          }}
-        >
-          {trackEvents.map((event) => (
-            <div
-              key={event.id}
-              className={styles.event}
-              data-type={event.type}
-              style={{
-                background: event.color || 'white',
-                left: `${event.x1}px`,
-                top: `${-event.height / 2}px`,
-                width: `${event.width}px`,
-                height: `${event.height}px`,
-                transition: immediate
-                  ? 'none'
-                  : `left ${DEFAULT_CSS_TRANSITION}, width ${DEFAULT_CSS_TRANSITION}`,
-              }}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
+    <Fragment>
+      <div width={outerWidth} height={graphHeight} className={styles.Events}>
+        {tracksEventsWithCoordinates.map((trackEvents, index) => (
+          <div
+            key={index}
+            className={styles.track}
+            style={{
+              top: `${getTrackY(tracksEvents.length, index, graphHeight)}px`,
+            }}
+          >
+            {trackEvents.map((event) => (
+              <div
+                key={event.id}
+                className={cx(styles.event, {
+                  [styles.highlighted]: highlightedEvent && highlightedEvent.id === event.id,
+                })}
+                data-type={event.type}
+                style={{
+                  background: event.color || 'white',
+                  left: `${event.x1}px`,
+                  top: `${-event.height / 2}px`,
+                  width: `${event.width}px`,
+                  height: `${event.height}px`,
+                  transition: immediate
+                    ? 'none'
+                    : `left ${DEFAULT_CSS_TRANSITION}, width ${DEFAULT_CSS_TRANSITION}`,
+                }}
+                onMouseEnter={() => setHighlightedEvent(event)}
+                onMouseLeave={() => setHighlightedEvent()}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      {tooltipContainer &&
+        ReactDOM.createPortal(
+          <Tooltip highlightedEvent={highlightedEvent} outerScale={outerScale} />,
+          tooltipContainer
+        )}
+    </Fragment>
   )
 }
 
@@ -70,12 +136,15 @@ TracksEvents.propTypes = {
         end: PropTypes.number,
         id: PropTypes.string,
         type: PropTypes.string,
+        color: PropTypes.string,
+        description: PropTypes.string,
       })
     )
   ).isRequired,
   outerScale: PropTypes.func.isRequired,
   outerWidth: PropTypes.number.isRequired,
   graphHeight: PropTypes.number.isRequired,
+  tooltipContainer: PropTypes.instanceOf(Element).isRequired,
 }
 
 export default TracksEvents
